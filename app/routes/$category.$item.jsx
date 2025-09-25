59dafd276c1ca84a8f6218786d88e509ec551e00
WaficMikati <wrmikati@proton.me>
2025-09-24 20:22:55 -0500

    Getting there...

--- app/routes/$category.$item.jsx
+++ app/routes/$category.$item.jsx
@@ -1,24 +1,43 @@
-import { useLoaderData } from "react-router"
+import { useLoaderData, useParams } from "react-router";
 
 export async function loader({ params }) {
-  const index = params.item.split('-')[0]
-  const category = params.category
+  const index = params.item.split("-")[0];
+  const category = params.category;
 
-  const response = await fetch(`https://swapi.tech/api/${category}/${index}`)
-  const data = await response.json()
+  const response = await fetch(`https://swapi.tech/api/${category}/${index}`);
+  const data = await response.json();
 
-  return data.result.properties
+  return data.result.properties;
 }
 
 export default function Item() {
-  const data = useLoaderData()
-  const dataArr = Object.entries(data)
+  const params = useParams();
+  const data = useLoaderData();
+  const dataArr = Object.entries(data).filter(
+    ([k]) => !["created", "edited"].includes(k)
+  );
 
   return (
-    <>
-      {dataArr.map(([property, info], i) => (
-        <h3 key={i}>{`${property}: ${info}`}</h3>
-      ))}
-    </>
-  )
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 col-lg-8">
+          <img
+            className="img-fluid object-fit-cover"
+            src={`/images/${params.category}/${data.name
+              .toLowerCase()
+              .replace("é", "e")
+              .replace(" ", "-")}.jpg`}
+          />
+        </div>
+        <div className="col-12 col-lg-4">
+          {dataArr.map(([property, info], i) => (
+            <div className="row" key={i}>
+              <span className="col-3 text-capitalize">{property}</span>
+              <span className="col-9 text-capitalize">{info}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/routes/$category.jsx
+++ app/routes/$category.jsx
@@ -1,19 +1,31 @@
-import { Link, useLoaderData, useParams } from 'react-router'
-import { capitalizeWords } from '../utils/stringUtils'
+import { Link, useLoaderData, useParams } from "react-router";
+import { capitalizeWords } from "../utils/stringUtils";
+import { useState } from "react";
 
 export async function loader({ params }) {
-  const response = await fetch(`https://www.swapi.tech/api/${params.category}`)
-  const json = await response.json()
+  const response = await fetch(`https://www.swapi.tech/api/${params.category}`);
+  const json = await response.json();
 
-  return json
+  return json;
 }
 
 export default function Category() {
-  const category = useParams().category
-  const data = useLoaderData()
-  // console.log(data.results || data.result)
+  const category = useParams().category;
+  const data = useLoaderData();
 
-  if (category === 'films') {
+  const [items, setItems] = useState(data.results || []);
+  const [next, setNext] = useState(data.next);
+
+  async function loadMore() {
+    if (!next) return;
+    const res = await fetch(next);
+    const json = await res.json();
+
+    setItems((prev) => [...prev, ...(json.results || [])]);
+    setNext(json.next);
+  }
+
+  if (category === "films") {
     return (
       <>
         <h1>{category}</h1>
@@ -21,36 +33,65 @@ export default function Category() {
           <div key={i}>
             <br />
             <h2>{e.properties.title}</h2>
-            {Object.entries(e.properties).filter(e => ['title', 'episode_id', 'director', 'producer', 'release_date', 'opening_crawl'].includes(e[0])).map(([type, info], i) => (
-              <h3 key={i}>{`${type}: ${info}`}</h3>
-            ))}
+            {Object.entries(e.properties)
+              .filter(([k]) =>
+                [
+                  "title",
+                  "episode_id",
+                  "director",
+                  "producer",
+                  "release_date",
+                  "opening_crawl",
+                ].includes(k)
+              )
+              .map(([type, info], i) => (
+                <h3 key={i}>{`${type}: ${info}`}</h3>
+              ))}
           </div>
         ))}
       </>
-    )
-  } else {
-    return (
-      <>
-        <h1>{category}</h1>
-        <div className='container my-5'>
-          <div className='row g-3'>
-            {data.results.map((e, i) => (
-              <div className='col-4' key={i}>
-                <Link to={`${e.url.split('/').pop()}-${e.name.replace(' ', '')}`}>
-                  <div className='card'>
-                    <div className='ratio ratio-4x3'>
-                      <img className='card-img-top object-fit-cover' src={`/images/${category}/${e.name.toLowerCase().split(' ').join('-')}.jpg`} />
-                    </div>
-                    <div className='card-body fs-5'>
-                      <span>{capitalizeWords(e.name)}</span>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            ))}
-          </div>
-        </div>
-      </>
-    )
+    );
   }
+
+  return (
+    <>
+      <h1 className="display-4 text-center text-capitalize">{category}</h1>
+      <div className="container my-5">
+        <div className="row g-3 justify-content-center">
+          {items.map((e, i) => (
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={i}>
+              <Link to={`${e.url.split("/").pop()}-${e.name.replace(" ", "")}`}>
+                <div className="card overflow-hidden">
+                  <div className="ratio ratio-4x3">
+                    <img
+                      className="card-img-top object-fit-cover"
+                      src={`/images/${category}/${e.name
+                        .toLowerCase()
+                        .replace("é", "e")
+                        .split(" ")
+                        .join("-")}.jpg`}
+                      alt={e.name}
+                    />
+                  </div>
+                  <div className="card-body align-content-end text-end fs-5 bg-black bg-gradient bg-op h-25 mt-auto p-2">
+                    <span className="card-text text-capitalize">{e.name}</span>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+          {next && (
+            <div className="text-center mt-5 ">
+              <button
+                className="btn btn-outline-warning fs-5"
+                onClick={loadMore}
+              >
+                Load More
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
